85980708209e1ae5eb6cd45ad8dc27c35be5a702
LEGENDARY_KING <aarushsalgania06@gmail.com>
2022-03-20 20:12:53 +0530

    made the bot

--- README.md
+++ README.md
@@ -2,21 +2,28 @@ A discord bot to view PDFs directly in discord. No more downloading hundereds of
 
 This bot used a modified version of https://www.npmjs.com/package/pdf-to-png-converter
 
-The changes are:
-
+The changes are in \node_modules\pdf-to-png-converter\out\convert.to.png.js:
 
 Line 41
+
 ```javascript
-   /* if (!props?.outputFileMask && isBuffer) {
+/* if (!props?.outputFileMask && isBuffer) {
         throw Error('outputFileMask is required when input is a Buffer.');
     } */
 ```
+
 And
 
 Line 82
 
 ```javascript
-        let pageName;
-        if (!isBuffer) pageName = path_1.parse(pdfFilePathOrBuffer).name;
-        if (!pageName) pageName = props?.outputFileMask ?? "No_File_Name";
+let pageName;
+if (!isBuffer) pageName = path_1.parse(pdfFilePathOrBuffer).name;
+if (!pageName) pageName = props?.outputFileMask ?? "No_File_Name";
+```
+
+Line 116 addition
+
+```javascript
+      totalPages: pdfDocument.numPages,
 ```
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const client = new Discord.Client({
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const { clientId, guildId } = require("./config.json");
+const { clientId, guildId, token } = require("./config.json");
+const pdfToPng = require("pdf-to-png-converter").pdfToPng;
+const fetch = require("node-fetch");
 
 let commands = [
   new SlashCommandBuilder()
@@ -17,14 +19,15 @@ let commands = [
         .setDescription("The link of the PDF")
         .setRequired(true)
     )
-    .addBooleanOption((option) =>
+    .addIntegerOption((option) =>
       option
-        .setName("ephemeral")
-        .setDescription("Should the output be ephemeral")
+        .setName("page")
+        .setDescription("Page number of the PDF (Defaults to 1)")
+        .setMinValue(0)
     ),
 ];
 
-const rest = new REST({ version: "9" }).setToken(process.env.token);
+const rest = new REST({ version: "9" }).setToken(token);
 
 rest
   .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
@@ -34,8 +37,150 @@ rest
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
   if (interaction.commandName === "read") {
+    page = interaction.options.getInteger("page") || 1;
+    await interaction.deferReply({ ephemeral: true });
+    try {
+      let fileSizeRes = await fetch(interaction.options.getString("link"), {
+        method: "HEAD",
+      });
+      let size = fileSizeRes.headers.get("content-length"),
+        type = fileSizeRes.headers.get("content-type");
+      if (size > 100 * 1e6)
+        return interaction.editReply({
+          content: "File size too big. Limit 100 MBs",
+          ephemeral: true,
+        });
+      if (type !== "application/pdf")
+        return interaction.editReply({
+          content: "Link received not of a PDF file",
+          ephemeral: true,
+        });
+      interaction.editReply({
+        content: "Downloading... This might take a bit depending on the size",
+        ephemeral: true,
+      });
+      let res = await fetch(interaction.options.getString("link"));
+      let response = await res.blob();
+      interaction.editReply({
+        content: "Downloaded! Processing the PDF",
+        ephemeral: true,
+      });
+      let arrayBuffer = await response.arrayBuffer();
+      let pdfBuffer = Buffer.from(arrayBuffer);
+      let pageFile = await getPage(pdfBuffer, page);
+      let totalPages = pageFile.totalPages;
+      let components = new Discord.MessageActionRow().setComponents([
+        new Discord.MessageButton()
+          .setStyle("SUCCESS")
+          .setEmoji("◀")
+          .setCustomId("previous"),
+        new Discord.MessageButton()
+          .setStyle("SECONDARY")
+          .setDisabled(true)
+          .setLabel("Page " + page + "/" + pageFile.totalPages)
+          .setCustomId("."),
+        new Discord.MessageButton()
+          .setStyle("SUCCESS")
+          .setEmoji("▶")
+          .setCustomId("next"),
+        new Discord.MessageButton()
+          .setStyle("DANGER")
+          .setLabel("End")
+          .setCustomId("end"),
+      ]);
+      let attachment = new Discord.MessageAttachment(
+        pageFile.content,
+        "Page.png"
+      );
+      let embed = new Discord.MessageEmbed()
+        .setColor("#99ff00")
+        .setImage("attachment://Page.png")
+        .setFooter({ text: "Used by " + interaction.user.tag });
+      try {
+        let message = await interaction.channel.send({
+          files: [attachment],
+          embeds: [embed],
+          components: [components],
+        });
 
+        const filter = (i) => i.user.id === interaction.user.id;
+        const collector = message.createMessageComponentCollector({
+          filter,
+        });
+        collector.on("collect", async (i) => {
+          if (i.customId === "previous") page--;
+          if (i.customId === "next") page++;
+          if (i.customId === "end") {
+            collector.stop();
+            i.deferUpdate();
+            return;
+          }
+          if (page < 1 || page > totalPages) {
+            if (page < 1) page = 1;
+            if (page > totalPages) page = totalPages;
+            return i.reply("Uh-oh. You have reached the end of the pdf");
+          }
+          await i.deferUpdate();
+          let pageFile = await getPage(pdfBuffer, page);
+          let attachment = new Discord.MessageAttachment(
+            pageFile.content,
+            "Page.png"
+          );
+          components.components[1].setLabel(
+            "Page " + page + "/" + pageFile.totalPages
+          );
+          await message.edit({
+            files: [attachment],
+            embeds: [embed],
+            components: [components],
+          });
+        });
+        collector.on("end", (collected) => {
+          let components2 = new Discord.MessageActionRow().setComponents([
+            new Discord.MessageButton()
+              .setDisabled(true)
+              .setStyle("SUCCESS")
+              .setEmoji("◀")
+              .setCustomId("previous"),
+            ,
+            new Discord.MessageButton()
+              .setStyle("SECONDARY")
+              .setDisabled(true)
+              .setLabel("Page " + page + "/" + pageFile.totalPages)
+              .setCustomId("."),
+            ,
+            new Discord.MessageButton()
+              .setDisabled(true)
+              .setStyle("SUCCESS")
+              .setEmoji("▶")
+              .setCustomId("next"),
+            ,
+          ]);
+          embed.setTitle("Will not reply anymore");
+          message.edit({ embeds: [embed], components: [components2] });
+        });
+      } catch (e) {
+        interaction.editReply({
+          content:
+            "I could not send the file. Please check if i have perms here",
+          ephemeral: true,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      interaction.channel.send({ content: "Uh-oh, Unknown error" });
+    }
   }
 });
 
-client.login(process.env.token).then(() => {});
+async function getPage(pdfBuffer, page) {
+  let pages = await pdfToPng(pdfBuffer, {
+    disableFontFace: false,
+    useSystemFonts: true,
+    viewportScale: 2.0,
+    pages: [page],
+  });
+  return pages[0];
+}
+
+client.login(token).then(() => {});
