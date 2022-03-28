dc49315905b7784d1220e706235fd528cd5277a3
LEGENDARY_KING <aarushsalgania06@gmail.com>
2022-03-29 02:34:20 +0530

    Changed nextPage to previousPage type

--- events/messageInteractionCreate.js
+++ events/messageInteractionCreate.js
@@ -3,24 +3,25 @@ module.exports = {
   description: "Manages all the slash commands initiation",
   once: false,
   async execute(interaction) {
-    if (!interaction.isApplicationCommand() && !interaction.isCommand()) return;
-    let command = interaction.client.messageCommands.get(
-      interaction.commandName
-    );
-    if (interaction.commandName === "read blank")
-      command = interaction.client.messageCommands.get("read");
-    if (!command) return;
+    if (interaction.isApplicationCommand() && !interaction.isCommand()) {
+      let command = interaction.client.messageCommands.get(
+        interaction.commandName
+      );
+      if (interaction.commandName === "read blank")
+        command = interaction.client.messageCommands.get("read");
+      if (!command) return;
 
-    try {
-      if (interaction.commandName !== "read blank")
-        await command.execute(interaction);
-      else await command.execute(interaction, true);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
+      try {
+        if (interaction.commandName !== "read blank")
+          await command.execute(interaction);
+        else await command.execute(interaction, true);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      }
     }
   },
 };
